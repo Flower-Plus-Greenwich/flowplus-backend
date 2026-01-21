6310a49bcff1306826e2f54a1c239193fd210031
phamthang44 <Ducthang4342@gmail.com>
2026-01-21 09:26:57 +0700

    fix(test): add application-test.yml and disable flyway/enable ddl-auto for tests

--- src/main/java/com/greenwich/flowerplus/dto/request/LoginRequest.java
+++ src/main/java/com/greenwich/flowerplus/dto/request/LoginRequest.java
@@ -3,6 +3,7 @@ package com.greenwich.flowerplus.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +13,7 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@AllArgsConstructor
 public class LoginRequest {
 
     @NotBlank(message = "Email is required")
